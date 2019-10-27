1aeca6c94ae591c6ac5eaf681528b25f22da893c
avielb <aviel33@gmail.com>
2019-10-28 00:09:05 +0200

    init commit

--- vars/centosRunner.groovy
+++ vars/centosRunner.groovy
@@ -1,5 +1,5 @@
 def call(String podLabel, code) {
-	podTemplate(containers: [containerTemplate(name: 'centos', image: 'centos:7', ttyEnabled: true, command: '/bin/bash')]) {
+	podTemplate(containers: [containerTemplate(name: 'centos', image: 'centos', ttyEnabled: true)]) {
 		code()
 	}
 }
